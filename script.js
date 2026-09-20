db7e030d95640cabc5e4e41617f0b7789dcc4dc7
zayan <zayan.rehman.ansari@gmail.com>
2026-09-19 23:06:52 -0400

    minour ui changes

---
+++ script.js
@@ -0,0 +1,19 @@
+const startScreen = document.getElementById("startScreen");
+const instructionScreen =document.querySelector(".instructionScreen");
+const instructionButton = document.querySelector(".instructionButton");
+const backButtonInstructionScreen = document.querySelector(".BackButtonInstructionScreen");
+const gameScreen = document.querySelector(".game");
+const startButton = document.querySelector(".startButton");
+
+
+instructionButton.addEventListener("click", function() {
+    startScreen.style.display = "none";
+    instructionScreen.style.display="block";});    
+
+backButtonInstructionScreen.addEventListener("click", function() {
+    instructionScreen.style.display="none"; 
+    startScreen.style.display = "block"});
+
+startButton.addEventListener("click", function(){
+    startScreen.style.display="none";
+    gameScreen.style.display = "block";});
